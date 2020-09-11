24d8fdd11c7aa751fe5ba55cc8286c511b092002
azhdanov <azhdanov@gmail.com>
2020-09-11 08:26:56 +0000

    b1464 #d0b3cc

--- test/jasmine/controller/timesheetControllerTest.js
+++ test/jasmine/controller/timesheetControllerTest.js
@@ -50,7 +50,7 @@ describe('timesheetControllerTest', function () {
   var checkOptions = function (scope) {
     expect(scope.groupByOptions).not.toBeNull()
     expect(scope.groupByOptions).toBeInstanceOf(TimesheetSelectOptions)
-    expect(scope.groupByOptions.options.length).toBe(scope.pivotTableType === 'TimeTracking' ? 40 : scope.pivotTableType === 'IssueWorkedTimeByUser' ? 47 : 46)
+    expect(scope.groupByOptions.options.length).toBe(scope.pivotTableType === 'TimeTracking' ? 39 : scope.pivotTableType === 'IssueWorkedTimeByUser' ? 46 : 45)
     expect(scope.groupByOptions.options).toContainInProperty('Security Level', 'label')
     expect(scope.groupByOptions.options).toContainInProperty('resolution', 'id')
     expect(scope.filterByOptions).not.toBeNull()
